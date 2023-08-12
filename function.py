f4683d8c93aa1db95e40be583aa8c3b2935ac3f3
adityakumar79915 <adityakumar79915@gmail.com>
2023-08-12 04:53:16 -0700

    updated

--- function.py
+++ function.py
@@ -1,12 +1,12 @@
 #function is a block of code that perform a particular task
 #argument value is substituted for name
-def power(x,n):
-     ans=1
-     for i in range(x,n):
-        ans=ans*x
-     return(ans)    
-y=power(3,5)
-print(y) 
+# def power(x,n):
+#      ans=1
+#      for i in range(x,n):
+#         ans=ans*x
+#      return(ans)    
+# y=power(3,5)
+# print(y) 
 
 
 
@@ -17,3 +17,11 @@ print(y)
 # v=stupid(28)
 # print(v)
 # print(n)
+
+#a function must be declared before it invoked
+def f(x):
+    return(g(x+1))
+def g(y):
+    return(y+3)
+z=f(77)
+print(z)
