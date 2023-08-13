c761e40d84cca2e6e9bed19c5865effdd0ff1a7b
adityakumar79915 <adityakumar79915@gmail.com>
2023-08-13 02:40:44 -0700

    updated

---
+++ recursion.py
@@ -0,0 +1,16 @@
+#when a function is calling itself is called recursion
+'''def factorial(n):
+    if n<=0:
+        return(1)
+    else:
+        return(n*factorial(n-1))
+print(factorial(5))    
+        '''
+        
+def factors(n):
+    factorslist=[]
+    for i in  range(1,n+1):
+        if n%i==0:
+            factorslist=factorslist+[i]
+    return(factorslist)
+print(factors(8))            
