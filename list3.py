60cdbd4935e97d0f5b5fd5037a6e1ee264b7980c
adityakumar79915 <adityakumar79915@gmail.com>
2023-08-16 05:59:51 -0700

    updated:

---
+++ list3.py
@@ -0,0 +1,14 @@
+#split 
+'''abc='aditya kumar sinha'
+x=abc.split()
+print(x)
+print(len(x))
+print(x[0])
+print(x)
+for i in x:
+    print(i)'''
+    
+line='a boy    in jungle'
+x=line.split()
+print(x)    
+print(len(x))
