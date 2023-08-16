8e069b42966654ca8138fac4916ca1061e4cfaa1
adityakumar79915 <adityakumar79915@gmail.com>
2023-08-16 05:50:33 -0700

    updated

---
+++ list manupulate.py
@@ -0,0 +1,32 @@
+# concatenate string
+'''a=[1,2,3]
+b=[4,5,6]
+print(a+b)'''
+
+#slicing
+'''a=[1,2,3]
+print(a[1:3])'''
+
+'''l=[]
+l.append('aditya')
+l.append('sinha')
+print(l)
+l.append('kumar')
+print(l)'''
+
+'''some=[1,2,3,4,5]
+print(5 in some)'''
+
+l=['kuamr','aditya']
+l.sort
+print(l)
+
+'''numlist=list()
+while True:
+    inp=input('enter a number')
+    if inp=='done':
+        break
+    value=float(inp)
+    numlist.append(value)
+average=sum(numlist)/len(numlist)
+print('average',average)    '''
