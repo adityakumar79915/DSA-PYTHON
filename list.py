54a8999f8b122f746e1b77a0bc74a38b66de96ab
adityakumar79915 <adityakumar79915@gmail.com>
2023-08-16 05:02:22 -0700

    updated

---
+++ list.py
@@ -0,0 +1,19 @@
+#ALGORITHM : a set of rules or steps used to solve a problem
+#DATA STRUCTURE : A particular way of organising data in a computer
+#LIST : a collection allows us to put many values in a single variable
+#list is denoted by [] brackets
+
+friends=['aditya','kumar','sinha']
+print(friends)
+
+print([1,[1,4],[5]])
+
+for i in [5,4,3,2,1]:
+    print(i)
+print('blastoff')
+
+friends=['aditya','kumar','sinha']
+for friend in friends:
+    print('happy new year:',friend)
+print('done!')    
+
